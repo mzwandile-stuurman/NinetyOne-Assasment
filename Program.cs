47a0d371e1196b34a755ef754b6ae1eb6bc8225d
mzwandile-stuurman <stuurmanmzwandile@gmail.com>
2023-08-31 16:07:17 +0200

    refined solution

--- Program.cs
+++ Program.cs
@@ -7,29 +7,30 @@ string csvFilePath = "TestData.csv";
 string[] lines = File.ReadAllLines(csvFilePath); //read every line from the string
 int[] scores = new int[4] ;
 
-string input = "ABC3454 ";
-string output = string.Concat( input.Where( Char.IsDigit ) );
-
-Console.WriteLine(output);
-
-for (int i=0; i< lines.Length; i++)  // extract digits only 
+for (int i=1; i< lines.Length; i++)  // extract digits only 
 {
    
-   string number = string.Concat(lines[i].Where(Char.IsDigit));
+   string number = string.Concat(lines[i].ToString().Where(Char.IsDigit));
+   //Console.WriteLine(number);
     for(int j= 0; j< scores.Length; j++){
 
         scores[j] = int.Parse(number);
+
     }
 
 }
+
+
 
  void GetMaxScoe(int[] sourceArray)
 {
     int maxElement = sourceArray[0];
     for (int index = 1; index < sourceArray.Length; index++)  // parse in the scores array and get the maximum score
     {
-        if (sourceArray[index] > maxElement)
+        if (sourceArray[index] > maxElement){
             maxElement = sourceArray[index];
+        }
+            
     }
 
     Console.WriteLine(maxElement);
